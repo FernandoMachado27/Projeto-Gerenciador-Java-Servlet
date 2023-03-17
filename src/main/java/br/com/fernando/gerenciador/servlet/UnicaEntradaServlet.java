1095a470fa8e39feed29d7966a0628943c5d15d9
Fernando Machado Fernandes <fernando.fernandes@keeggo.com>
2023-03-17 11:18:08 -0300

    Escondendo o JSP

--- src/main/java/br/com/fernando/gerenciador/servlet/UnicaEntradaServlet.java
+++ src/main/java/br/com/fernando/gerenciador/servlet/UnicaEntradaServlet.java
@@ -13,6 +13,7 @@ import br.com.fernando.gerenciador.acao.AlteraEmpresa;
 import br.com.fernando.gerenciador.acao.ListaEmpresas;
 import br.com.fernando.gerenciador.acao.MostraEmpresa;
 import br.com.fernando.gerenciador.acao.NovaEmpresa;
+import br.com.fernando.gerenciador.acao.NovaEmpresaForm;
 import br.com.fernando.gerenciador.acao.RemoveEmpresa;
 
 @WebServlet("/entrada")
@@ -43,12 +44,16 @@ public class UnicaEntradaServlet extends HttpServlet { // controlador
 			NovaEmpresa acao = new NovaEmpresa();
 			nome = acao.executa(request, response);
 			
+		}else if (paramAcao.equals("NovaEmpresaForm")) {
+			NovaEmpresaForm acao = new NovaEmpresaForm();
+			nome = acao.executa(request, response);
+			
 		}
 		
 		String[] tipoEEndereco = nome.split(":"); // separa a String baseada no caractere
 		
 		if(tipoEEndereco[0].equals("forward")) { // se na primeira posição tem forward
-			RequestDispatcher rd = request.getRequestDispatcher(tipoEEndereco[1]); // então usa o valor
+			RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/view/" + tipoEEndereco[1]); // então usa o valor
 			rd.forward(request, response);
 		}else { 
 			response.sendRedirect(tipoEEndereco[1]); // navegador redireciona
